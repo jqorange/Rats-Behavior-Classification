034bbc5d972000a66e80ef26d776ff01826d60c4
Jiaqi_Shen <91200514+jqorange@users.noreply.github.com>
2025-10-29 11:53:07 -0400

    Update session list in train_new.py

--- train_new.py
+++ train_new.py
@@ -549,7 +549,7 @@ def main() -> None:
     args = parser.parse_args()
 
     data_root = r"D:\\Jiaqi\\Datasets\\Rats\\TrainData_new"
-    sessions = ["F3D5_outdoor", "F3D6_outdoor", "F5D2_outdoor", "F5D10_outdoor", "F6D5_outdoor_2"]
+    sessions = ["F3D5_outdoor", "F3D6_outdoor", "F5D2_outdoor", "F5D10_outdoor",  "F5D7_outdoor"]
     batch_size = 512
     session_ranges = None
 
